1d15069401a494add45318dd2e7d7a14cde214a8
Felippe <113641116+Felippe-nobre@users.noreply.github.com>
2025-03-24 13:27:05 -0300

    finalização do curso de MVC de php pela aluar

---
+++ php-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '87e8c3bcb9f67c9704d23ad241e77e8cb1b0dbb7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '87e8c3bcb9f67c9704d23ad241e77e8cb1b0dbb7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
